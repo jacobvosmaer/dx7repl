8cb5e6ff64b8b8dff8ecd34c01144fe179be53c7
Jacob Vosmaer <contact@jacobvosmaer.nl>
2019-03-08 11:08:01 +0100

    whitespace

--- lib/dx7/packed_voice.rb
+++ lib/dx7/packed_voice.rb
@@ -12,23 +12,23 @@ module DX7
         binvoice = bindata.bytes[(5 - i) * 17, 17]
         op_hash = {}
         j = 0
-      
+
         Operator::KEYS.each do |key|
           next if %i[rc pd ts pc].include?(key)
 
           op_hash[key] = binvoice[j]
           j += 1
         end
-      
+
         op_hash[:lc] = binvoice[11] & 0b11
         op_hash[:rc] = (binvoice[11] >> 2) & 0b11
-      
+
         op_hash[:rs] = binvoice[12] & 0b111
         op_hash[:pd] = binvoice[12] >> 3
-      
+
         op_hash[:ams] = binvoice[13] & 0b11
         op_hash[:ts] = (binvoice[13] >> 2) & 0b1111
-      
+
         op_hash[:pm] = binvoice[15] & 0b1
         op_hash[:pc] = (binvoice[15] >> 1) & 0b11111
 
@@ -40,16 +40,16 @@ module DX7
       j = 102
       Voice::KEYS.each do |key|
         next if %i[opi lfw lpms].include?(key)
-      
+
         voice_hash[key] = binvoice[j]
         j += 1
       end
-      
+
       voice_hash[:als] = binvoice[110] & 0b11111
-      
+
       voice_hash[:fbl] = binvoice[111] & 0b111
       voice_hash[:opi] = (binvoice[111] >> 3) & 0b1
-      
+
       voice_hash[:lfks] = binvoice[116] & 0b1
       voice_hash[:lfw] = (binvoice[116] >> 1) & 0b111
       voice_hash[:lpms] = (binvoice[116] >> 4) & 0b111
